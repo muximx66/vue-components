e1ac059bcf4e146425ec9f196f3528affec85bb5
muximx66 <muximx66@163.com>
2019-10-12 10:56:55 +0800

    日历组件-有bug待修复

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- 日历组件 -->
+  <datePicker
+    :options="calendarArr"
+    class="calendar"
+    @handleClickDay="handleClickDay"
+    @handlePrevMonth="handlePrevMonth"
+    @handleNextMonth="handleNextMonth"
+  />
+</template>
+
+<script>
+// 日历组件
+import datePicker from "./components/data-picker";
+export default {
+  name: "date-picker",
+  data() {
+    return {
+      calendarArr: {
+        type: "combination",
+        headStyle: {
+          todayBtn: "right",
+          combination: "center",
+          checkBtn: "right"
+        },
+        viewStyle: {
+          day: "right"
+        },
+        calendarData: []
+      }
+    };
+  },
+  components: {
+    datePicker
+  },
+  methods: {
+    handleClickDay() {},
+    handlePrevMonth() {},
+    handleNextMonth() {}
+  }
+};
+</script>
+
+<style>
+</style>
